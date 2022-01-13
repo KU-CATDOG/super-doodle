913dd95b9058082e9626a0462e188b0ea5f7b889
MyBug18 <mskimkim0318@gmail.com>
2022-01-13 19:41:34 +0900

    주석 좀 추가함

--- Assets/Scripts/Earth.cs
+++ Assets/Scripts/Earth.cs
@@ -7,8 +7,14 @@ public class Earth : MonoBehaviour
 {
     private readonly HashSet<EarthObject> objects = new HashSet<EarthObject>();
 
+    /// <summary>
+    /// 이 땅 위에 올라와 있는 모든 오브젝트
+    /// </summary>
     public IReadOnlyCollection<EarthObject> EarthObjects => objects;
 
+    /// <summary>
+    /// 크기 (보여주기 용도)
+    /// </summary>
     public int Radius { get; private set; } = 5;
 
     [SerializeField]
@@ -19,6 +25,7 @@ public class Earth : MonoBehaviour
 
     public KeyCode CurrentKeyCode { get; private set; }
 
+    // 충돌 탐지를 위해서 이 땅을 몇 구획으로 쪼갤 것인가
     private const int collisionSystemDivisionNum = 12;
 
     private readonly HashSet<EarthObject>[] collisionInfo = new HashSet<EarthObject>[collisionSystemDivisionNum];
@@ -53,12 +60,16 @@ public class Earth : MonoBehaviour
     {
         obj.transform.parent = transform;
         objects.Add(obj);
+
+        UpdateCollisionInfo();
     }
 
     public void KillEarthObject(EarthObject obj)
     {
         objects.Remove(obj);
         Destroy(obj);
+
+        UpdateCollisionInfo();
     }
 
     private void OnSingleKeyEvent(IEvent e)
@@ -78,6 +89,7 @@ public class Earth : MonoBehaviour
         }
     }
 
+    // 새로운 키로 갱신한다.
     private void UpdateKeyCode()
     {
         CurrentKeyCode = KeyGenerator.GetKeyCode();
@@ -93,10 +105,14 @@ public class Earth : MonoBehaviour
 
         foreach (var i in objects)
         {
+            // 현재 오브젝트의 위치에 따라 인덱스를 정한다.
             collisionInfo[(int)(i.ClampedRadian / (Mathf.PI / (collisionSystemDivisionNum / 2)))].Add(i);
         }
     }
 
+    /// <summary>
+    /// 주어진 Radian 지점 앞뒤로 Radius 라디안 안에 있는 오브젝트를 전부 리턴한다.
+    /// </summary>
     public HashSet<EarthObject> ProbeEarthObject(float radian, float radius)
     {
         var result = new HashSet<EarthObject>();
@@ -104,6 +120,7 @@ public class Earth : MonoBehaviour
         radian = Mod(radian, Mathf.PI * 2);
         radius = Mathf.Clamp(radius, 0, Mathf.PI / 2 - 0.01f);
 
+        // 구획 탐색의 시작점과 끝 점을 구한다.
         var start = (int)Mathf.Floor((radian - radius) / (Mathf.PI / (collisionSystemDivisionNum / 2)));
         var end = (int)Mathf.Floor((radian + radius) / (Mathf.PI / (collisionSystemDivisionNum / 2)));
 
@@ -112,6 +129,8 @@ public class Earth : MonoBehaviour
             var isEdge = idx == start || idx == end;
 
             var i = idx;
+
+            // 무조건 0 ~ num 사이로 맞춰야 한다.
             if (i < 0)
             {
                 i += collisionSystemDivisionNum;
@@ -122,6 +141,7 @@ public class Earth : MonoBehaviour
                 i -= collisionSystemDivisionNum;
             }
 
+            // 끝자락에 있는 놈들은 안쪽에서 거리를 재서 나눈다.
             if (isEdge)
             {
                 foreach (var j in collisionInfo[i])
@@ -132,6 +152,7 @@ public class Earth : MonoBehaviour
                     result.Add(j);
                 }
             }
+            // 끝자락이 아니면 무조건 포함된다.
             else
             {
                 foreach (var j in collisionInfo[i])
