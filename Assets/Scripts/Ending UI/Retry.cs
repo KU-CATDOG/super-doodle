a1cc3b96c8b35dc3a36c25c9d0c49f0364ab6363
DY <dave1315@korea.ac.kr>
2022-02-07 22:09:52 +0900

    결과창에서 랜덤키를 눌러 재시작하게 수정

--- Assets/Scripts/Ending UI/Retry.cs
+++ Assets/Scripts/Ending UI/Retry.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using Tool;
 
 public class Retry : MonoBehaviour
 {
-    void Update()
+    KeyCode key;
+
+    [SerializeField]
+    TextMeshPro retryText;
+
+    private void Start()
+    {
+        key = EarthKeyGenerator.KeyGenerator.GetKeyCode();
+        retryText.text = "Press " + key + " To Retry";
+    }
+
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("MapSelect");
         }
-        else if (Input.anyKeyDown)
+        else if (Input.GetKeyDown(key))
+        {
             SceneManager.LoadScene("SampleScene");
+        }
     }
 }
