289aeec71571b6702b80f7ecf85e019af66fdd3e
cse21jh <cse21jh@korea.ac.kr>
2022-02-23 02:00:54 +0900

    메인화면 부드럽게 움직이게 + 각도 계산

--- Assets/Scripts/Ending UI/Menu.cs
+++ Assets/Scripts/Ending UI/Menu.cs
@@ -7,21 +7,20 @@ using TMPro;
 
 public class Menu : MonoBehaviour
 {
-    IEvent e;
     KeyCode key;
-    private float rotateSpeed = 1f;
+    private float rotateSpeed = 0.03f;
 
     [Header("GameObjects")]
     [SerializeField] private Transform Earth;
     [SerializeField] private Transform player;
     [SerializeField] private TextMeshPro nextKey;
 
+    [SerializeField] private TextMeshPro space;
     [SerializeField] GameObject playerObject;
 
     Quaternion angle = Quaternion.Euler(0, 0, 0);
     float curAngle = 0f;
-
-    public bool moving = false;
+    Vector2 start;
 
     void Start()
     {
@@ -29,34 +28,71 @@ public class Menu : MonoBehaviour
         key = gen.GetKeyCode();
         // key = EarthKeyGenerator.KeyGenerator.GetKeyCode();
         nextKey.text = EarthKeyGenerator.KeyGenerator.KeyCodeToString(key);
+
+        start = player.position;
     }
 
     void Update()
     {
+        Vector2 end = player.position;
+
+        player.RotateAround(Earth.position, Vector3.back, rotateSpeed);
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene("InitialScene");
         }
 
-        if (Input.GetKeyDown(key) && !moving)
+        if (Input.GetKeyDown(key))
         {
-            moving = true;
             // 플레이어 이동
-            curAngle -= 90f;
+            rotateSpeed += 0.01f;
+
+            /*curAngle -= 90f;
             angle = Quaternion.Euler(0, 0, curAngle);
-            StartCoroutine(RotatePlayer(angle));
+            StartCoroutine(RotatePlayer(angle));*/
 
             key = EarthKeyGenerator.KeyGenerator.GetKeyCode();
             nextKey.text = EarthKeyGenerator.KeyGenerator.KeyCodeToString(key);
         }
-        
-            //if 버튼에 도착할 경우
-            //초기 위치에서 90도, 180도, 270도
-            //if (Input.GetKeyDown(KeyCode.Space)) -> 해당 씬으로 이동
-  
+
+        curAngle = GetAngle(start, end);
+
+        //if 버튼에 도착할 경우
+        if (curAngle % 180 >= 42.5 || curAngle % 180 <= 47.5)
+        {
+            space.text = "Space";
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // SceneManager.LoadScene("Setting");
+            }
+        }
+        else if (curAngle % 180 >= 87.5 || curAngle % 180 <= 92.5)
+        {
+            space.text = "Space";
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("MapSelect");
+            }
+        }
+        else if (curAngle % 180 >= 132.5 || curAngle % 180 <= 137.5)
+        {
+            space.text = "Space";
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // SceneManager.LoadScene("Credit");
+            }
+        }
+            
     }
 
-    IEnumerator RotatePlayer(Quaternion angle)
+    float GetAngle(Vector2 start, Vector2 end)
+    {
+        Vector2 v2 = end - start;
+        return 0 - (Mathf.Atan2(v2.y, v2.x) * Mathf.Rad2Deg);
+    }
+
+    /*IEnumerator RotatePlayer(Quaternion angle)
     {
         Quaternion tAngle;
         for(float t=0; t<= 0.55f; t+= Time.deltaTime)
@@ -66,5 +102,5 @@ public class Menu : MonoBehaviour
             yield return null;
         }
         moving = false;
-    }
+    }*/
 }
