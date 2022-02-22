b0613e2812bca93e7040b427881ad2dc37307c24
HyunjinLee <hyunjin1109@korea.ac.kr>
2022-02-22 15:02:14 +0900

    메뉴화면 수정

--- Assets/Scripts/Ending UI/Menu.cs
+++ Assets/Scripts/Ending UI/Menu.cs
@@ -16,6 +16,13 @@ public class Menu : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private TextMeshPro nextKey;
 
+    [SerializeField] GameObject playerObject;
+
+    Quaternion angle = Quaternion.Euler(0, 0, 0);
+    float curAngle = 0f;
+
+    public bool moving = false;
+
     void Start()
     {
         var gen = new RandomKeyGenerator();
@@ -31,17 +38,33 @@ public class Menu : MonoBehaviour
             SceneManager.LoadScene("InitialScene");
         }
 
-        if (Input.GetKeyDown(key))
+        if (Input.GetKeyDown(key) && !moving)
         {
-                // 플레이어 이동
-                player.RotateAround(Earth.position, Vector3.back, 30f);
-                key = EarthKeyGenerator.KeyGenerator.GetKeyCode();
-                nextKey.text = EarthKeyGenerator.KeyGenerator.KeyCodeToString(key);
+            moving = true;
+            // 플레이어 이동
+            curAngle -= 90f;
+            angle = Quaternion.Euler(0, 0, curAngle);
+            StartCoroutine(RotatePlayer(angle));
+
+            key = EarthKeyGenerator.KeyGenerator.GetKeyCode();
+            nextKey.text = EarthKeyGenerator.KeyGenerator.KeyCodeToString(key);
         }
         
             //if 버튼에 도착할 경우
             //초기 위치에서 90도, 180도, 270도
             //if (Input.GetKeyDown(KeyCode.Space)) -> 해당 씬으로 이동
   
+    }
+
+    IEnumerator RotatePlayer(Quaternion angle)
+    {
+        Quaternion tAngle;
+        for(float t=0; t<= 0.55f; t+= Time.deltaTime)
+        {
+            tAngle = playerObject.transform.rotation;
+            playerObject.transform.rotation = Quaternion.Slerp(tAngle, angle, 0.1f);
+            yield return null;
+        }
+        moving = false;
     }
 }
